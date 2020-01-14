057f023f1b260ec9d43deabec10d6fe461591096
garyA2C <garyA2C@gmail.com>
2020-01-14 16:49:34 +0100

    Modification regle

--- ServerBoardManager.py
+++ ServerBoardManager.py
@@ -20,113 +20,121 @@ class ServerPlayer:
 class Board:
 
     def __init__(self, *player_names):
-        self.deck = Board.generate_shuffled_deck()
-        #self.current_card = self.deck.pop()
+        self.available_colors=['red','blue']
+        self.available_values=[i for i in range(1,10)]
+        self.available_cards=[]
+        for color in self.available_colors :
+            for value in self.available_values :
+                self.available_cards+=[[color,value]]
+        self.deck = Board.generate_shuffled_deck(self.available_colors,self.available_values)
         self.size_grid = 7
-        self.grid = [[[False,False,None]] * self.size_grid for i in range(self.size_grid)]
-        update_board(self.size_grid//2,self.size_grid//2,self.deck.pop())
+        self.grid = [[None]*self.size_grid for i in range(self.size_grid)]
+        self.grid[self.size_grid//2][self.size_grid//2] = self.deck.pop()
         self.players = [ServerPlayer(player_names[j], [self.deck.pop() for i in range(5)]) for j in
                         range(len(player_names))]
 
     @staticmethod
-    def generate_shuffled_deck():
-        deck = [('red', i // 2) if i % 2 == 0 else ('blue', i // 2) for i in range(2, 22)]
+    def generate_shuffled_deck(colors,values):
+        deck=[]
+        for color in colors :
+            for value in values :
+                deck+=[[color,value]]
         random.shuffle(deck)
         return deck
 
-    def update_board(self,x,y,new_card):
-        self.grid[x][y][0]=True
-        self.grid[x][y][1]=True
-        self.grid[x][y][2]=new_card
-        try :
-            if self.grid[x-1][y][1]==False:
-                self.grid[x-1][y][0]=True
-        except IndexError:
-            pass
-        try :
-            if self.grid[x+1][y][1]==False:
-                self.grid[x+1][y][0]=True
-        except IndexError:
-            pass
-        try :
-            if self.grid[x][y-1][1]==False:
-                self.grid[x][y-1][0]=True
-        except IndexError:
-            pass
-        try :
-            if self.grid[x][y+1][1]==False:
-                self.grid[x][y+1][0]=True
-        except IndexError:
-            pass
-
-    def update_player_hand_from_move(self, player_id, played_card):
+    def update_player_hand_from_move_on(self, x, y, player_id, played_card):
         """
-        Simule le dépot par ce joueur de cette carte.
-        Si le coup est permis, la carte est enlevée de la main de ce joueur et devient la carte courante.
+        Tente le dépot d'une carte sur une case par un joueur.
+        Si le coup est permis, la carte est enlevée de la main de ce joueur et est posée sur la grille.
         Sinon, le joueur prend une carte du deck dans sa main.
         """
-        if self.is_card_playable(played_card):
-            self.players[player_id].hand.remove(played_card)
-            self.current_card = played_card
+        if self.is_card_playable_on(x,y,played_card):   #Teste si la carte peut être posée
+            self.players[player_id].hand.remove(played_card)    #Si oui, on enlève la carte de la main
+            self.grid[x][y]=played_card  #Et on la pose sur le plateau
             print(self.players[player_id].hand)
         else:
-            self.players[player_id].hand.append(self.deck.pop())
+            self.players[player_id].hand.append(self.deck.pop())    #Sinon il pioche
 
-    def is_card_playable_on(self, played_card, x, y):
-        color = played_card[0]
-        value = played_card[1]
+    def is_box_filled(self,x,y):
+        return self.grid[x][y]!=None
 
-        is_playable=self.grid[x][y][0]
-        is_filled=self.grid[x][y][1]
-        case_color=self.grid[x][y][2][0]
-        case_value=self.grid[x][y][2][1]
+    def is_box_empty(self,x,y):
+        return self.grid[x][y]==None
 
-        if is_playable:
-            if is_filled:
-                if case_color!=color:
-                    return value==case_value
-                else :
+    def is_index_not_out_of_range(self,a):
+        return a % (self.size_grid-1) == a
+
+    def get_number_of_filled_neighbour(self,x,y):
+        filled_neighbour=0
+        if self.is_index_not_out_of_range(x-1):
+            if self.is_box_filled(x-1,y):
+                filled_neighbour+=1
+        if self.is_index_not_out_of_range(x+1):
+            if self.is_box_filled(x+1,y):
+                filled_neighbour+=1
+        if self.is_index_not_out_of_range(y-1):
+            if self.is_box_filled(x,y-1):
+                filled_neighbour+=1
+        if self.is_index_not_out_of_range(y+1):
+            if self.is_box_filled(x,y+1):
+                filled_neighbour+=1
+        return filled_neighbour
+
+    def is_box_playable(self,x,y):
+        if self.is_box_empty(x,y):
+            return self.get_number_of_filled_neighbour(x,y)>=1
+        else :
+            return self.get_number_of_filled_neighbour(x,y)==1
+
+    def get_color_of(self,x,y):
+        return self.grid[x][y][0]
+
+    def get_value_of(self,x,y):
+        return self.grid[x][y][1]
+
+    def is_card_playable_on(self, x, y, played_card):
+        """
+        Simule le dépot d'une carte à une position (x,y)
+        Renvoie True si le coup est permis, False sinon.
+        """
+        if self.is_box_playable(x,y):  #Si la case est jouable
+            if self.is_box_filled(x,y):  #Si la case est déjà remplie
+                if self.get_color_of(x,y) == played_card[0]:   #Si la couleur de la carte déjà posée est différente de celle de la carte à poser
                     return False
-            else :
-                return get_value_playable(x,y).contains(value)
-        return False
+                else :
+                    return self.get_value_of(x,y) == played_card[1] #On retourne True si la valeur de la carte déjà posée est la même que celle de la carte à poser
+            else :  #Si la case est vide
+                return played_card in self.get_cards_playable(x,y)  #On regarde si la carte respecte les règles
+        else :
+            return False
 
-    def get_value_playable(self,x,y):
-        impossible_values=[]
-        possible_values=[1,2,3,4,5,6,7,8,9]
-        try :
-            if grid[x-1][y][1]:
-                impossible_values+=[x for x in possible_values if x!=grid[x-1][y][2][1]-1 and x!=grid[x-1][y][2][1]+1]
-        except IndexError:
-            pass
-        try :
-            if grid[x+1][y][1]:
-                impossible_values+=[x for x in possible_values if x!=grid[x+1][y][2][1]-1 and x!=grid[x+1][y][2][1]+1]
-        except IndexError:
-            pass
-        try :
-            if grid[x][y-1][1]:
-                impossible_values+=[x for x in possible_values if x!=grid[x][y-1][2][1]-1 and x!=grid[x][y-1][2][1]+1]
-        except IndexError:
-            pass
-        try :
-            if grid[x][y+1][1]:
-                impossible_values+=[x for x in possible_values if x!=grid[x][y+1][2][1]-1 and x!=grid[x][y+1][2][1]+1]
-        except IndexError:
-            pass
-        return [i for i in possible_values if not i in impossible_values]
+    def get_cards_playable(self,x,y):
+        """
+        Cette méthode est appelée sur une position vide et jouable de la grille.
+        Elle renvoie l'ensemble des cartes pouvant être posées en respectant les règles du jeu
 
-    def is_card_playable(self, played_card):
+        On parcours la liste des cartes du jeux
+        On défini comme impossible les cartes qui ont :
+        -Une couleur différente de celle de la carte de droite, de gauche, du haut ou du bas
+        -Une valeur différente de la valeurs ± 1 de celle de la carte de droite, de gauche, du haut ou du bas
         """
-        Vérifie si cette carte peut être jouée au dessus d'une position du board.
-        Renvoie True si oui, False sinon
-        """
-        color = played_card[0]
-        value = played_card[1]
-        if color != self.current_card[0]:  # Si même couleur
-            return value == self.current_card[1] - 1 or played_card[1] == self.current_card[1] + 1  # Vrai si valeur =  +/- 1 courante
-        else:
-            return value == self.current_card[1]  # Vrai si valeur = celle courante
+        impossible_cards=[]
+
+        if self.is_index_not_out_of_range(x-1):
+            if self.is_box_filled(x-1,y):
+                impossible_cards+=[card for card in self.available_cards if card[0]!=self.get_color_of(x-1,y) or card[1]!=(self.get_value_of(x-1,y)-1) and card[1]!=(self.get_value_of(x-1,y)+1)]
+        if self.is_index_not_out_of_range(x+1):
+            if self.is_box_filled(x+1,y):
+                impossible_cards+=[card for card in self.available_cards if card[0]!=self.get_color_of(x+1,y) or card[1]!=(self.get_value_of(x+1,y)-1) and card[1]!=(self.get_value_of(x+1,y)+1)]
+        if self.is_index_not_out_of_range(y-1):
+            if self.is_box_filled(x,y-1):
+                impossible_cards+=[card for card in self.available_cards if card[0]!=self.get_color_of(x,y-1) or card[1]!=(self.get_value_of(x,y-1)-1) and card[1]!=(self.get_value_of(x,y-1)+1)]
+        if self.is_index_not_out_of_range(y+1):
+            if self.is_box_filled(x,y+1):
+                impossible_cards+=[card for card in self.available_cards if card[0]!=self.get_color_of(x,y+1) or card[1]!=(self.get_value_of(x,y+1)-1) and card[1]!=(self.get_value_of(x,y+1)+1)]
+
+        #On retourne toutes les cartes du jeu qui ne sont pas impossibles
+        return [card for card in self.available_cards if not card in impossible_cards]
 
     def is_game_over(self):
         for hand in self.players.hand:
@@ -143,7 +151,8 @@ class Board:
     def get_client_data_for(self, player_id):
         cdata = ClientData()
         cdata.player_hand = self.players[player_id].hand
-        cdata.current_card = self.current_card
+        cdata.size_grid=self.size_grid
+        cdata.grid=self.grid
         cdata.deck_size = len(self.deck)
         cdata.other_players.clear()
         for i in range(len(self.players)):
@@ -152,10 +161,31 @@ class Board:
         return cdata
 
 
-
-
 if __name__ == "__main__":
-    board = Board('Jean', 'Robert', 'Fabrice')
+    board = Board('Jean')
+
     print(board.deck)
-    print(board.current_card)
-    print(board.players[1].hand)
+    for i in range(board.size_grid):
+        for j in range(board.size_grid):
+            print(str(i)+"-"+str(j)+" "+str(board.grid[i][j]))
+    print(board.players[0].hand)
+
+    """#pour tester, mettre en commentaire random.shuffle(deck)
+    board.update_player_hand_from_move_on(3, 3, 0, ['blue',6])
+    board.update_player_hand_from_move_on(3, 3, 0, ['blue',6])
+    board.update_player_hand_from_move_on(3, 3, 0, ['blue',6])
+    board.update_player_hand_from_move_on(3, 3, 0, ['blue',6])
+    board.update_player_hand_from_move_on(3, 3, 0, ['blue',6])
+    board.update_player_hand_from_move_on(4, 1, 0, ['blue',6])
+    board.update_player_hand_from_move_on(3, 3, 0, ['blue',6])
+    board.update_player_hand_from_move_on(3, 2, 0, ['blue',8])
+    board.update_player_hand_from_move_on(3, 1, 0, ['blue',7])
+    board.update_player_hand_from_move_on(4, 1, 0, ['blue',6])
+    board.update_player_hand_from_move_on(4, 1, 0, ['red',6])
+    board.update_player_hand_from_move_on(4, 2, 0, ['red',7])
+    board.update_player_hand_from_move_on(4, 0, 0, ['red',7])
+
+    for i in range(board.size_grid):
+        for j in range(board.size_grid):
+            print(str(i)+"-"+str(j)+" "+str(board.grid[i][j]))
+    print(board.players[0].hand)"""
